c3567c56bed5ceb938bff0cefda8ca06c74bc2e4
Casey Muller <github@nullterminated.com>
2018-06-20 13:41:59 -0700

    Add alerts to catalog app

--- cmd/catalog/catalog.go
+++ cmd/catalog/catalog.go
@@ -7,7 +7,38 @@ import (
 	"github.com/caseymrm/menuet"
 )
 
-var notifications = []menuet.Notification{
+var alertsCatalog = []menuet.Alert{
+	menuet.Alert{
+		MessageText: "Just MessageText",
+	},
+	menuet.Alert{
+		InformativeText: "Just InformativeText",
+	},
+	menuet.Alert{
+		MessageText:     "MessageText and InformativeText",
+		InformativeText: "This is the InformativeText",
+	},
+	menuet.Alert{
+		MessageText: "Message and two buttons",
+		Buttons:     []string{"One", "Two"},
+	},
+	menuet.Alert{
+		MessageText: "Message and input",
+		Inputs:      []string{"Example input"},
+	},
+	menuet.Alert{
+		MessageText:     "Message, InformativeText, Button, and Input",
+		InformativeText: "Example InformativeText",
+		Buttons:         []string{"Example button"},
+		Inputs:          []string{"Example Input"},
+	},
+	menuet.Alert{
+		MessageText: "Message and two inputs",
+		Inputs:      []string{"Input one", "Input two"},
+	},
+}
+
+var notificationsCatalog = []menuet.Notification{
 	menuet.Notification{
 		Title: "Just a title",
 	},
@@ -60,8 +91,20 @@ var notifications = []menuet.Notification{
 }
 
 func menuItems() []menuet.MenuItem {
-	notifs := make([]menuet.MenuItem, 0, len(notifications))
-	for ind, notif := range notifications {
+	alerts := make([]menuet.MenuItem, 0, len(alertsCatalog))
+	for ind, alert := range alertsCatalog {
+		text := alert.MessageText
+		if text == "" {
+			text = alert.InformativeText
+		}
+		alerts = append(alerts, menuet.MenuItem{
+			Text:     text,
+			Callback: fmt.Sprintf("alert %d", ind),
+		})
+	}
+
+	notifs := make([]menuet.MenuItem, 0, len(notificationsCatalog))
+	for ind, notif := range notificationsCatalog {
 		text := notif.Title
 		if text == "" {
 			text = notif.Subtitle
@@ -71,10 +114,15 @@ func menuItems() []menuet.MenuItem {
 		}
 		notifs = append(notifs, menuet.MenuItem{
 			Text:     text,
-			Callback: fmt.Sprintf("notif-%d", ind),
+			Callback: fmt.Sprintf("notif %d", ind),
 		})
 	}
+
 	return []menuet.MenuItem{
+		menuet.MenuItem{
+			Text:     "Show Alert",
+			Children: alerts,
+		},
 		menuet.MenuItem{
 			Text:     "Send Notification",
 			Children: notifs,
@@ -85,14 +133,20 @@ func menuItems() []menuet.MenuItem {
 func handleClicks(callback chan string) {
 	for click := range callback {
 		var index int
-		n, err := fmt.Sscanf(click, "notif-%d", &index)
+		var kind string
+		n, err := fmt.Sscan(click, &kind, &index)
 		if err != nil {
 			log.Printf("Sscanf error: %v", err)
 			continue
 		}
-		if n == 1 {
-			menuet.App().Notification(notifications[index])
+		if n != 2 {
 			continue
+		}
+		switch kind {
+		case "notif":
+			menuet.App().Notification(notificationsCatalog[index])
+		case "alert":
+			menuet.App().Alert(alertsCatalog[index])
 		}
 	}
 }
