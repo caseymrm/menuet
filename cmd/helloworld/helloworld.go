d00972ba3beca41b88c5d343ea6359439592ddd8
Casey Muller <github@nullterminated.com>
2018-07-23 21:19:39 -0700

    Remove debug code from helloworld

--- cmd/helloworld/helloworld.go
+++ cmd/helloworld/helloworld.go
@@ -11,20 +11,11 @@ func helloClock() {
 		menuet.App().SetMenuState(&menuet.MenuState{
 			Title: "Hello World " + time.Now().Format(":05"),
 		})
-		menuet.App().MenuChanged()
 		time.Sleep(time.Second)
 	}
 }
 
 func main() {
 	go helloClock()
-	menuet.App().MenuOpened = func(key string) []menuet.MenuItem {
-		return []menuet.MenuItem{
-			{
-				Text:     time.Now().Format(":05"),
-				Children: true,
-			},
-		}
-	}
 	menuet.App().RunApplication()
 }
