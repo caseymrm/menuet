7789d5f4d291db09b09f15b965c85bb3373621f9
Casey Muller <github@nullterminated.com>
2018-08-15 10:05:09 -0700

    Image cache

--- NSImage+Resize.m
+++ NSImage+Resize.m
@@ -1,5 +1,51 @@
 #import "NSImage+Resize.h"
 
+@interface ImageCache : NSObject
++ (ImageCache *)instance;
+- (void)setImage:(NSImage *)image
+         forName:(NSString *)name
+      withHeight:(CGFloat)height;
+- (NSImage *)getImageForName:(NSString *)name withHeight:(CGFloat)height;
+@property(nonatomic, strong) NSCache *imageCache;
+@end
+
+static ImageCache *instance;
+
+@implementation ImageCache
+
++ (ImageCache *)instance {
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    instance = [[ImageCache alloc] init];
+  });
+  return instance;
+}
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    self.imageCache = [[NSCache alloc] init];
+  }
+  return self;
+}
+
+- (NSString *)keyForName:(NSString *)name withHeight:(CGFloat)height {
+  return [NSString stringWithFormat:@"%f/%@", height, name];
+}
+
+- (void)setImage:(NSImage *)image
+         forName:(NSString *)name
+      withHeight:(CGFloat)height {
+  [self.imageCache setObject:image
+                      forKey:[self keyForName:name withHeight:height]];
+}
+
+- (NSImage *)getImageForName:(NSString *)name withHeight:(CGFloat)height {
+  return
+      [self.imageCache objectForKey:[self keyForName:name withHeight:height]];
+}
+
+@end
+
 @implementation NSImage (Resize)
 
 - (NSImage *)imageWithHeight:(CGFloat)height {
@@ -27,7 +73,11 @@
   if (name.length == 0) {
     return nil;
   }
-  NSImage *image = nil;
+  NSImage *image =
+      [[ImageCache instance] getImageForName:name withHeight:height];
+  if (image != nil) {
+    return image;
+  }
   if ([name hasPrefix:@"http"]) {
     image = [[NSImage alloc] initWithContentsOfURL:[NSURL URLWithString:name]];
   } else {
@@ -36,6 +86,7 @@
   if (height > 0 && image.size.height > height) {
     image = [image imageWithHeight:height];
   }
+  [[ImageCache instance] setImage:image forName:name withHeight:height];
   return image;
 }
 
